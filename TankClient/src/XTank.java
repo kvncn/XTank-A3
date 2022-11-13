92ff8bffbea218fbdea69b0b71691eac0ab03921
KateN1996 <98198608+KateN1996@users.noreply.github.com>
2022-11-12 19:04:31 -0700

    Update XTank.java
    comments

--- TankClient/src/XTank.java
+++ TankClient/src/XTank.java
@@ -1,11 +1,23 @@
 import java.net.Socket;
 import java.io.DataOutputStream;
 import java.io.DataInputStream;
-
+/**
+ * @author Kevin Nisterenko
+ * FILE: XTank.java
+ * ASSIGNMENT: A3 - XTank
+ * COURSE: CSC 335; FALL 2022
+ * PURPOSE: This class creates the client for XTank.
+ * 
+ * This class sets up the in and out data streams along with the ui
+ * for the client. 
+ *
+ */
 public class XTank 
 {
 	public static void main(String[] args) throws Exception 
     {
+		//can be modified to be played over network and not just local
+		//compile time flexibility according to David
         try (var socket = new Socket("127.0.0.1", 59896)) 
         {
         	DataInputStream in = new DataInputStream(socket.getInputStream());
@@ -15,4 +27,3 @@ public class XTank
         }
     }
 }
-
